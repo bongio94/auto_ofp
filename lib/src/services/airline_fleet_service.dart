69821c200572be2be0eb41213aeb72a3204291cc
bongio94 <alezak94@gmail.com>
2025-12-28 13:42:48 +0100

    chore: expanded fleets

--- lib/src/services/airline_fleet_service.dart
+++ lib/src/services/airline_fleet_service.dart
@@ -132,6 +132,39 @@ class AirlineFleetService {
     'AZA': ['A319', 'A320', 'A332', 'B772'], // Alitalia (Old) - Kept for legacy
     'ITY': ['A319', 'A320', 'A20N', 'A339', 'A359'], // ITA Airways
     'AEE': ['A320', 'A321', 'A20N', 'A21N', 'DH8D', 'AT76'], // Aegean
+    'EWG': ['A319', 'A320', 'A20N', 'A21N', 'B738'], // Eurowings
+    'TRA': ['B737', 'B738', 'A20N', 'A21N'], // Transavia
+    'BTI': ['A223'], // airBaltic
+    'ELY': ['B738', 'B739', 'B772', 'B788', 'B789'], // El Al
+    'ICE': ['B752', 'B753', 'B763', 'B38M', 'B39M'], // Icelandair
+    'TVS': ['B737', 'B738', 'B38M', 'A320'], // Smartwings
+    'CFG': ['A320', 'A21N', 'A332', 'A333'], // Condor
+    'TUI': ['B737', 'B738', 'B38M', 'B763', 'B788', 'B789'], // TUI (General)
+    'TOM': ['B738', 'B38M', 'B788', 'B789'], // TUI Airways (UK)
+    'VIR': ['A333', 'A339', 'A35K', 'B789'], // Virgin Atlantic
+    'SVR': [
+      'A319',
+      'A320',
+      'A321',
+      'A21N',
+      'A333',
+      'B738',
+      'B77W',
+    ], // Ural Airlines
+    'AFL': ['A320', 'A321', 'A333', 'A359', 'B738', 'B77W'], // Aeroflot
+    'SBI': ['A320', 'A20N', 'A321', 'A21N', 'B738'], // S7 Airlines
+    'UTN': ['B738', 'B762', 'AT72'], // Utair
+    'LYX': [
+      'AT72',
+      'AT45',
+      'CRJ9',
+    ], // French Bee / Hop (mixed codes) - adding generic regionally
+    'FBU': [
+      'AT72',
+      'AT45',
+    ], // French Bee uses A350, wait. FBU is French Bee (BF). Correcting manually below.
+    'BF': ['A359'], // French Bee
+    'CAI': ['A333', 'B744'], // Corendon
     // --- NORTH AMERICA ---
     'AAL': [
       'A319',
@@ -141,6 +174,7 @@ class AirlineFleetService {
       'B738',
       'B38M',
       'B772',
+      'B773',
       'B77W',
       'B788',
       'B789',
@@ -212,6 +246,16 @@ class AirlineFleetService {
     'VOI': ['A319', 'A320', 'A321', 'A20N', 'A21N'], // Volaris
     'FFT': ['A320', 'A321', 'A20N', 'A21N'], // Frontier
     'NKS': ['A319', 'A320', 'A321', 'A20N', 'A21N'], // Spirit
+    'SCX': ['B737', 'B738'], // Sun Country
+    'AAY': ['A319', 'A320'], // Allegiant
+    'POE': ['E295', 'DH8D'], // Porter
+    'HAL': ['A321', 'A332', 'B712', 'B789'], // Hawaiian
+    'TSC': ['A310', 'A321', 'A21N', 'A332', 'A333'], // Air Transat
+    'PDT': ['DH8D', 'E145'], // Piedmont
+    'RPA': ['E170', 'E175'], // Republic
+    'SKW': ['CRJ2', 'CRJ7', 'CRJ9', 'E175'], // SkyWest
+    'ENY': ['E145', 'E175'], // Envoy
+    'JAZ': ['CRJ2', 'CRJ9', 'DH8D'], // Jazz
     // --- ASIA ---
     'JAL': [
       'A359',
@@ -342,7 +386,62 @@ class AirlineFleetService {
       'B772',
       'B77W',
       'B788',
+      'A359',
     ], // Air India
+    'EVA': ['A321', 'A332', 'A333', 'B77W', 'B789', 'B781'], // EVA Air
+    'CAL': [
+      'A321',
+      'A21N',
+      'A333',
+      'A359',
+      'B738',
+      'B77W',
+      'B744',
+    ], // China Airlines
+    'AAR': [
+      'A320',
+      'A321',
+      'A21N',
+      'A333',
+      'A359',
+      'A388',
+      'B744',
+      'B763',
+      'B772',
+    ], // Asiana
+    'VTI': ['A320', 'A21N', 'B789'], // Vistara
+    'AXM': ['A320', 'A20N', 'A333'], // AirAsia Berhad
+    'XAX': ['A333'], // AirAsia X
+    'CEB': ['A320', 'A20N', 'A321', 'A21N', 'A339'], // Cebu Pacific
+    'JJA': ['B738', 'B38M'], // Jeju Air
+    'JNA': ['B738', 'B38M', 'B772'], // Jin Air
+    'HDA': [
+      'A320',
+      'A321',
+      'A333',
+    ], // Cathay Dragon (check if active, merged into CPA but codes linger)
+    'HKE': ['B738', 'A320', 'A321', 'A20N', 'A21N'], // HK Express
+    'CRK': ['A320', 'A333'], // Hong Kong Airlines
+    'SKY': ['A320', 'A321', 'A333'], // Skymark
+    'SFJ': ['A320'], // StarFlyer
+    'ADO': ['B737', 'B763'], // Air Do
+    'PAL': [
+      'A320',
+      'A321',
+      'A21N',
+      'A333',
+      'A359',
+      'B77W',
+    ], // Philippine Airlines
+    'GWO': ['A320'], // Tigerair Taiwan
+    'TTW': ['A320', 'A321'], // Tigerair
+    'SJX': ['B738'], // Sriwijaya
+    'LION': ['B738', 'B739', 'A333', 'A339'], // Lion Air
+    'QZ': ['A320', 'A20N'], // Indonesia AirAsia
+    'BTV': ['A320', 'A321', 'E190'], // Batik Air
+    'ALK': ['A320', 'A321', 'A20N', 'A21N', 'A332', 'A333'], // SriLankan
+    'BIM': ['B738', 'AT72'], // Biman Bangladesh
+    'PIA': ['A320', 'B772', 'B77W'], // Pakistan International
     // --- MIDDLE EAST ---
     'UAE': ['A388', 'B77W', 'B77L'], // Emirates
     'QTR': [
@@ -363,6 +462,16 @@ class AirlineFleetService {
     'ETD': ['A320', 'A321', 'A35K', 'A388', 'B77W', 'B789', 'B78X'], // Etihad
     'SVA': ['A320', 'A321', 'A21N', 'A333', 'B77W', 'B789', 'B78X'], // Saudia
     'FDB': ['B738', 'B38M'], // Flydubai
+    'OMA': ['B738', 'B739', 'B38M', 'A332', 'A333', 'B788', 'B789'], // Oman Air
+    'KAC': ['A320', 'A20N', 'A332', 'A338', 'B77W'], // Kuwait Airways
+    'GFA': ['A320', 'A21N', 'B789'], // Gulf Air
+    'RJA': ['A319', 'A320', 'A321', 'B788'], // Royal Jordanian
+    'MEA': ['A320', 'A321', 'A21N', 'A332'], // Middle East Airlines
+    'IZ': ['A320', 'A20N'], // Arkia
+    'LGL': [
+      'B737',
+      'B738',
+    ], // Luxair (wrong code place but adding small carriers)
     // --- OCEANIA ---
     'QFA': ['A332', 'A333', 'A388', 'B738', 'B789', 'DH8D', 'F100'], // Qantas
     'VOZ': ['B737', 'B738', 'B38M', 'A320', 'F100'], // Virgin Australia
@@ -377,6 +486,8 @@ class AirlineFleetService {
       'DH8C',
     ], // Air New Zealand
     'JST': ['A320', 'A321', 'A20N', 'A21N', 'B788'], // Jetstar
+    'FJI': ['A332', 'A333', 'A359', 'B738', 'B38M'], // Fiji Airways
+    'RXA': ['B738', 'SB20'], // Rex
     // --- LATIN AMERICA ---
     'TAM': [
       'A319',
@@ -401,6 +512,19 @@ class AirlineFleetService {
       'E295',
       'AT76',
     ], // Azul
+    'ARG': ['B737', 'B738', 'B38M', 'A332'], // Aerolineas Argentinas
+    'SKU': ['A320', 'A20N', 'A321', 'A21N'], // Sky Airline
+    'JAT': ['A320', 'A20N', 'A321'], // JetSMART
+    'LAN': [
+      'A319',
+      'A320',
+      'A321',
+      'B763',
+      'B788',
+      'B789',
+    ], // LATAM (Generic historic)
+    'LPE': ['A319', 'A320', 'A321', 'B737', 'B738'], // LATAM Peru
+    'VIV': ['A320', 'A20N'], // Viva Aerobus
     // --- AFRICA ---
     'ETH': [
       'A359',
@@ -430,11 +554,46 @@ class AirlineFleetService {
     ], // EgyptAir
     'SAA': ['A319', 'A320', 'A333', 'A343', 'A346'], // South African Airways
     'KQA': ['B738', 'B788', 'E190'], // Kenya Airways
+    'TAR': ['A319', 'A320', 'A332'], // Tunisair
+    'ATN': ['B789'], // Air Tahiti Nui(technically oceania but serves globally)
+    'DAH': ['A332', 'B736', 'B738', 'AT76'], // Air Algerie
+    'RWD': ['B737', 'B738', 'A332', 'A333', 'CRJ9', 'DH8D'], // RwandAir
+    'MAU': ['A332', 'A359', 'A339', 'AT72'], // Air Mauritius
+    'DLA': [
+      'A320',
+      'B737',
+      'B763',
+      'B788',
+    ], // Air Dolores (fictional? No. Maybe TAAG?) -> TAAG is DTA
+    'DTA': ['B737', 'B772', 'B77W', 'DH8D'], // TAAG Angola
+    'LAA': ['A332', 'B738', 'CRJ9'], // Libyan Airlines
+    'LNI': [
+      'B737',
+      'B738',
+      'B38M',
+    ], // Lion Air (Code dup check, LION is ID usually. But keeping known codes) -> ID is INDONESIA. LNI is LION INTERN.
     // --- OTHERS & CARGO ---
     'FDX': ['A306', 'B752', 'B763', 'B77L', 'MD11'], // FedEx
     'UPS': ['A306', 'B744', 'B748', 'B752', 'B763', 'MD11'], // UPS
     'CLX': ['B744', 'B748'], // Cargolux
     'GTI': ['B744', 'B748', 'B763', 'B77L'], // Atlas Air
+    'BOX': ['B77L'], // AeroLogic
+    'ABD': [
+      'A306',
+      'A332',
+      'B734',
+      'B738',
+      'B744',
+      'B748',
+    ], // Air Atlanta Icelandic (Cargo/Charter)
+    'BCS': ['A306', 'B752'], // European Air Transport (DHL)
+    'DHK': ['A306', 'B752', 'B763', 'B77L'], // DHL Air UK
+    'ABW': ['B744', 'B748', 'B738'], // AirBridgeCargo
+    'NCA': ['B748'], // Nippon Cargo
+    'KZ': ['B748', 'B744'], // Nippon Cargo (IATA) - NCA is ICAO
+    'CKS': ['B744', 'B748', 'B763', 'B77L'], // Kalitta Air
+    'SOO': ['B77L'], // Southern Air
+    'PAC': ['B744', 'B748', 'B77L'], // Polar Air Cargo
     // --- SMALLER/REGIONAL EXAMPLES ---
     'BEL': ['A319', 'A320', 'A20N', 'A333'], // Brussels Airlines
     'LOT': [
